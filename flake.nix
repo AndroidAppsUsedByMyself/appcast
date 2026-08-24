0c16918d0d888ba5c100f102261a8664ae746392
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2026-08-24 15:33:44 +0800

    chore: bump version to 0.1.1

--- flake.nix
+++ flake.nix
@@ -25,7 +25,7 @@
         {
           default = pkgs.rustPlatform.buildRustPackage {
             pname = "appcast";
-            version = "0.1.0";
+            version = "0.1.1";
             src = ./.;
             cargoLock.lockFile = ./Cargo.lock;
             meta = with pkgs.lib; {
